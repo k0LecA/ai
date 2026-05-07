d5d142ec96c3b2925474f288511a4e0c24d6bc2c
k0LecA <54716798+k0LecA@users.noreply.github.com>
2026-05-07 18:09:25 +0300

    works ye

--- oddoreven.cpp
+++ oddoreven.cpp
@@ -27,8 +27,28 @@ double sigmoid(double x) {
 int main(){
 
     int input=1;
-
+    vector<double> h={0,0,0,0};
+    double bias=0;
+    double out;
+    vector<double> w;
+    vector<double> outw;
+    vector<double> d;
+    vector<double> wd;
+    vector<double> outwd;
+    double od;
+    vector<double> in;
+    double lr=0.25;
+    vector<double> hbias(4, 0.0);
+    double outbias = 0.0;
+    in.resize(8);
+    for(int i=0;i<in.size()*h.size();i++){
+        w.push_back(((double)rand() / (RAND_MAX)) * 2.0 - 1.0);
+    }
+    for(int i=0;i<h.size();i++){
+        outw.push_back(((double)rand() / (RAND_MAX)) * 2.0 - 1.0);
+    }
     while(input>0){
+        for(int i=0;i<h.size();i++) h[i]=0;
         cout<<endl<<"Input from 1 to 255: ";
         cin>>input;
 
@@ -42,18 +62,11 @@ int main(){
         }
 
 
-        vector<double> in=decToBinary(input);
+        in=decToBinary(input);
         for(int i=0;i<in.size();i++){
             cout<<in[i]<<" ";
         }
         
-
-        vector<double> h={0,0,0,0};
-        double bias=0;
-        vector<double> w;
-        for(int i=0;i<in.size()*h.size();i++){
-            w.push_back(((double)rand() / (RAND_MAX)) * 2.0 - 1.0);
-        }
         cout<<endl<<w.size()<<endl;
 
         //for(int i=0;i<in.size()*h.size();i++){
@@ -61,20 +74,13 @@ int main(){
         //}
 
         for(int i=0;i<h.size();i++){
-            h[i]+=bias;
+            h[i]+=hbias[i];
             for(int j=0;j<in.size();j++){
                 h[i]+=in[j]*w[i*in.size()+j];
             }
             h[i]=relu(h[i]);
         }
-
-        double outbias=0;
-        double out=0;
-        vector<double> outw;
-        for(int i=0;i<h.size();i++){
-            outw.push_back(((double)rand() / (RAND_MAX)) * 2.0 - 1.0);
-        }
-
+        outbias += lr * od;
         out=outbias;
         for(int i=0;i<h.size();i++){
             out+=h[i]*outw[i];
@@ -86,5 +92,42 @@ int main(){
 
         double error=target-out;
         cout<<"error: "<<error<<endl;
+
+        if(round(out)==1)
+            cout<<endl<<"odd!!!";
+        else if(round(out)==0)
+            cout<<endl<<"even!!!";
+        else
+            cout<<"somewhere is error";
+
+        od=out*(1-out)*error;
+
+        d.clear();
+        d.resize(h.size());
+        for(int i=0;i<h.size();i++)
+        {
+            //d[i]=h[i]*(1-h[i])*(outw[i]*od);
+            d[i] = (h[i] > 0 ? 1.0 : 0.0) * (outw[i] * od);
+        }
+
+        outwd.clear();
+        outwd.resize(outw.size());
+        for(int i=0;i<outw.size();i++)
+        {
+            outwd[i]=lr*od*h[i];
+            outw[i]+=outwd[i];
+        }
+
+        wd.clear();
+        wd.resize(w.size());
+        for(int i=0; i<h.size(); i++)
+        {
+            hbias[i] += lr * d[i];
+            for(int j=0; j<in.size(); j++)
+            {
+                int weightIndex = i * in.size() + j;
+                w[weightIndex] += lr * d[i] * in[j];
+            }
+        }
     }
 }
